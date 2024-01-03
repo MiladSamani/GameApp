0991203d4a5485845524ad112decbf5415dac03a
Milad Samani <eng.milad.samani@gmail.com>
2024-01-03 23:42:20 +0330

    implement mysql user methods

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"gameAppProject/entity"
+	"gameAppProject/repository/mysql"
+)
 
 func main() {
-	fmt.Println("Hello world, This is a new project!")
+
+}
+
+func testUserMysqlRepo() {
+	mysqlRepo := mysql.New()
+	createdUser, err := mysqlRepo.Register(entity.User{ID: 0, PhoneNumber: "09384571370", Name: "Milad Samani"})
+	if err != nil {
+		fmt.Println("can't created register user", err)
+	} else {
+		fmt.Println("created user : ", createdUser)
+	}
+	isUnique, err := mysqlRepo.IsPhoneNumberUnique(createdUser.PhoneNumber + "23")
+	if err != nil {
+		fmt.Println("unique err", err)
+	}
+
+	fmt.Println("isUnique", isUnique)
 }
---
+++ repository/mysql/user.go
@@ -0,0 +1,34 @@
+package mysql
+
+import (
+	"database/sql"
+	"fmt"
+	"gameAppProject/entity"
+)
+
+func (d *MySQLDB) IsPhoneNumberUnique(phoneNumber string) (bool, error) {
+	user := entity.User{}
+	var createdAt []uint8
+	// QueryRow executes a query that is expected to return at most one row.
+	// If the query selects no rows, the *Row's Scan will return ErrNoRows.
+	row := d.db.QueryRow(`select * from users where phone_number =?`, phoneNumber)
+	err := row.Scan(&user.ID, &user.Name, &user.PhoneNumber, &createdAt)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return true, nil
+		}
+		return false, fmt.Errorf("can't scan query result : %w", err)
+	}
+	return false, nil
+}
+
+func (d *MySQLDB) Register(u entity.User) (entity.User, error) {
+	res, err := d.db.Exec(`insert into users(name, phone_number) values(?,?)`, u.Name, u.PhoneNumber)
+	if err != nil {
+		return entity.User{}, fmt.Errorf("can't execute command %w", err)
+	}
+	//error is always nil
+	id, _ := res.LastInsertId()
+	u.ID = uint(id)
+	return u, nil
+}
