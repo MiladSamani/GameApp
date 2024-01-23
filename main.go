42ad5a1132458b84395bc2fc2965bca32dc7541c
MiladSamani <eng.milad.samani@gmail.com>
2024-01-23 18:14:39 +0330

    add migrator

--- main.go
+++ main.go
@@ -19,8 +19,6 @@ const (
 )
 
 func main() {
-	fmt.Println("start echo server")
-
 	cfg := config.Config{
 		HTTPServer: config.HTTPServer{Port: 8088},
 		Auth: authservice.Config{
@@ -39,10 +37,15 @@ func main() {
 		},
 	}
 
+	// TODO - add command for migrations
+	//mgr := migrator.New(cfg.Mysql)
+	//mgr.Up()
+
 	authSvc, userSvc := setupServices(cfg)
 
 	server := httpserver.New(cfg, authSvc, userSvc)
 
+	fmt.Println("start echo server")
 	server.Serve()
 }
 
--- repository/mysql/user.go
+++ repository/mysql/user.go
@@ -4,59 +4,72 @@ import (
 	"database/sql"
 	"fmt"
 	"gameAppProject/entity"
+	"time"
 )
 
 func (d *MySQLDB) IsPhoneNumberUnique(phoneNumber string) (bool, error) {
-	row := d.db.QueryRow(`select * from users where phone_number =?`, phoneNumber)
+	row := d.db.QueryRow(`select * from users where phone_number = ?`, phoneNumber)
+
 	_, err := scanUser(row)
 	if err != nil {
 		if err == sql.ErrNoRows {
 			return true, nil
 		}
-		return false, fmt.Errorf("can't scan query result : %w", err)
+
+		return false, fmt.Errorf("can't scan query result: %w", err)
 	}
+
 	return false, nil
 }
 
 func (d *MySQLDB) Register(u entity.User) (entity.User, error) {
-	res, err := d.db.Exec(`insert into users(name, phone_number , password) values(?,?,?)`, u.Name, u.PhoneNumber, u.Password)
+	res, err := d.db.Exec(`insert into users(name, phone_number, password) values(?, ?, ?)`, u.Name, u.PhoneNumber, u.Password)
 	if err != nil {
-		return entity.User{}, fmt.Errorf("can't execute command %w", err)
+		return entity.User{}, fmt.Errorf("can't execute command: %w", err)
 	}
-	//error is always nil
+
+	// error is always nil
 	id, _ := res.LastInsertId()
 	u.ID = uint(id)
+
 	return u, nil
 }
 
 func (d *MySQLDB) GetUserByPhoneNumber(phoneNumber string) (entity.User, bool, error) {
-	row := d.db.QueryRow(`select * from users where phone_number =?`, phoneNumber)
+	row := d.db.QueryRow(`select * from users where phone_number = ?`, phoneNumber)
 
 	user, err := scanUser(row)
 	if err != nil {
 		if err == sql.ErrNoRows {
 			return entity.User{}, false, nil
 		}
-		return entity.User{}, false, fmt.Errorf("can't scan query result : %w", err)
+
+		return entity.User{}, false, fmt.Errorf("can't scan query result: %w", err)
 	}
+
 	return user, true, nil
 }
 
 func (d *MySQLDB) GetUserByID(userID uint) (entity.User, error) {
-	row := d.db.QueryRow(`select * from users where id =?`, userID)
+	row := d.db.QueryRow(`select * from users where id = ?`, userID)
+
 	user, err := scanUser(row)
 	if err != nil {
 		if err == sql.ErrNoRows {
 			return entity.User{}, fmt.Errorf("record not found")
 		}
-		return entity.User{}, fmt.Errorf("can't scan query result : %w", err)
+
+		return entity.User{}, fmt.Errorf("can't scan query result: %w", err)
 	}
+
 	return user, nil
 }
 
 func scanUser(row *sql.Row) (entity.User, error) {
-	var createdAt []uint8
+	var createdAt time.Time
 	var user entity.User
+
 	err := row.Scan(&user.ID, &user.Name, &user.PhoneNumber, &createdAt, &user.Password)
+	fmt.Println("createdAt", createdAt)
 	return user, err
 }
